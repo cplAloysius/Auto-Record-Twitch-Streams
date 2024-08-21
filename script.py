b627451786f5312abfd1c5a51007dc2ec91c4e8c
Aloysius <diamondhead85@gmail.com>
2024-08-21 12:46:39 +0800

    replace user literal with variable obtained from json

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ import json
 with open('config.json', 'r') as f:
 	config = json.load(f)
 
+my_username = config.get('username')
 client_id = config.get('client_id')
 app_access_token = config.get('app_access_token')
 url = config.get('url')
@@ -31,7 +32,7 @@ while(True):
 	if stream_info["data"]:
 		timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
 		output_file = f"stream_{timestamp}.mp4"
-		os.system(f"sudo -u aloysiusloh streamlink --twitch-disable-ads twitch.tv/{streamer_username} best -o '{save_path}{output_file}'")
+		os.system(f"sudo -u {my_username} streamlink --twitch-disable-ads twitch.tv/{streamer_username} best -o '{save_path}{output_file}'")
 		continue
 	
 	time.sleep(60)
